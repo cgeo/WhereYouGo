3cbb5a99765d610e93aceaa0efcb59bf999d7d3d
Lars <gc@droescher.eu>
2020-03-30 20:53:14 +0200

    Inject error to test codacy - Do not merge

--- src/main/java/menion/android/whereyougo/network/DownloadCartridgeTask.java
+++ src/main/java/menion/android/whereyougo/network/DownloadCartridgeTask.java
@@ -39,9 +39,9 @@ import okhttp3.Response;
 
 public class DownloadCartridgeTask extends
         AsyncTask<String, DownloadCartridgeTask.Progress, Boolean> {
-    private static final String TAG = "DownloadCartridgeTask";
-    private static final String LOGIN = "https://www.wherigo.com/login/default.aspx";
-    private static final String DOWNLOAD = "https://www.wherigo.com/cartridge/download.aspx";
+    private static String TAG = "DownloadCartridgeTask";
+    private static String LOGIN = "https://www.wherigo.com/login/default.aspx";
+    private static String DOWNLOAD = "https://www.wherigo.com/cartridge/download.aspx";
 
     private final String username;
     private final String password;
