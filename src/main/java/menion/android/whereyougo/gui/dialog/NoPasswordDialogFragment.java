3bbeaf698043423b005414677b08055a4cdc6183
SchoolGuy <matrixfueller@gmail.com>
2020-05-29 08:51:38 +0200

    Address @bekuno s review wishes

--- src/main/java/menion/android/whereyougo/gui/dialog/NoPasswordDialogFragment.java
+++ src/main/java/menion/android/whereyougo/gui/dialog/NoPasswordDialogFragment.java
@@ -3,9 +3,9 @@ package menion.android.whereyougo.gui.dialog;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
 import menion.android.whereyougo.R;
@@ -15,7 +15,7 @@ public class NoPasswordDialogFragment extends DialogFragment {
     private NoPasswordDialogListener listener;
 
     public interface NoPasswordDialogListener {
-        public void onPositiveClick(DialogFragment dialog);
+        void onPositiveClick(DialogFragment dialog);
     }
 
     @Override
@@ -29,24 +29,17 @@ public class NoPasswordDialogFragment extends DialogFragment {
         }
     }
 
+    @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setMessage(R.string.dialog_no_password)
-                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        listener.onPositiveClick(NoPasswordDialogFragment.this);
-                    }
-                })
-                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // User cancelled the dialog --> Do nothing
-                    }
+                .setPositiveButton(R.string.ok, (dialog, id) -> listener.onPositiveClick(NoPasswordDialogFragment.this))
+                .setNegativeButton(R.string.cancel, (dialog, id) -> {
+                    // User cancelled the dialog --> Do nothing
                 });
         // Create the AlertDialog object and return it
         return builder.create();
     }
-
-
 }
