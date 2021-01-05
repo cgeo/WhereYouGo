a8b2fbeb5bcf5b13f8e68a9a70067c494f9ad911
SchoolGuy <matrixfueller@gmail.com>
2021-01-05 13:47:22 +0100

    Try to simplify more complex onCreatePreference Methods

--- src/main/java/menion/android/whereyougo/gui/fragments/settings/SettingsLocationFragment.java
+++ src/main/java/menion/android/whereyougo/gui/fragments/settings/SettingsLocationFragment.java
@@ -9,9 +9,12 @@ import androidx.preference.PreferenceFragmentCompat;
 
 import menion.android.whereyougo.R;
 import menion.android.whereyougo.preferences.Preferences;
+import menion.android.whereyougo.utils.Logger;
 import menion.android.whereyougo.utils.Utils;
 
 public class SettingsLocationFragment extends PreferenceFragmentCompat {
+    private static final String TAG = SettingsGlobalFragment.class.getSimpleName();
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.whereyougo_preferences_location, rootKey);
@@ -21,8 +24,8 @@ public class SettingsLocationFragment extends PreferenceFragmentCompat {
         CheckBoxPreference allowGps = findPreference(Preferences.getKey(R.string.pref_KEY_B_GPS));
         EditTextPreference adjustAltitude = findPreference(Preferences.getKey(R.string.pref_KEY_S_GPS_ALTITUDE_MANUAL_CORRECTION));
         CheckBoxPreference beepOnGpsFix = findPreference(Preferences.getKey(R.string.pref_KEY_B_GPS_BEEP_ON_GPS_FIX));
-        CheckBoxPreference disableWhenHidden = findPreference(Preferences.getKey(R.string.pref_KEY_B_GPS_DISABLE_WHEN_HIDE));
-        CheckBoxPreference guidingGpsRequired = findPreference(Preferences.getKey(R.string.pref_KEY_B_GUIDING_GPS_REQUIRED));
+        prepareDisableWhenHidden();
+        prepareGuidingGpsRequired();
 
         if (allowGps != null) {
             allowGps.setOnPreferenceChangeListener((preference, o) -> {
@@ -45,6 +48,10 @@ public class SettingsLocationFragment extends PreferenceFragmentCompat {
                 return true;
             });
         }
+    }
+
+    private void prepareDisableWhenHidden() {
+        CheckBoxPreference disableWhenHidden = findPreference(Preferences.getKey(R.string.pref_KEY_B_GPS_DISABLE_WHEN_HIDE));
         if (disableWhenHidden != null) {
             disableWhenHidden.setOnPreferenceChangeListener((preference, o) -> {
                 boolean newValue = (boolean) o;
@@ -53,19 +60,29 @@ public class SettingsLocationFragment extends PreferenceFragmentCompat {
                     CheckBoxPreference status_bar = findPreference(Preferences.getKey(R.string.pref_KEY_B_STATUSBAR));
                     CheckBoxPreference gps_guideing = findPreference(Preferences.getKey(R.string.pref_KEY_B_GUIDING_GPS_REQUIRED));
                     CheckBoxPreference screen_off = findPreference(Preferences.getKey(R.string.pref_KEY_B_RUN_SCREEN_OFF));
-                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P && screen_off.isChecked()) {
-                        status_bar.setEnabled(false);
-                    } else {
-                        if (newValue) {
-                            status_bar.setEnabled(!gps_guideing.isChecked());
-                        } else {
+                    if (status_bar != null && gps_guideing != null && screen_off != null) {
+                        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P
+                            && screen_off.isChecked()) {
                             status_bar.setEnabled(false);
+                        } else {
+                            if (newValue) {
+                                status_bar.setEnabled(!gps_guideing.isChecked());
+                            } else {
+                                status_bar.setEnabled(false);
+                            }
                         }
+                    } else {
+                        Logger.e(TAG, "One of the required settings was missing. (KEY_B_STATUSBAR," +
+                            " KEY_B_GUIDING_GPS_REQUIRED, KEY_B_RUN_SCREEN_OFF)");
                     }
                 }
                 return true;
             });
         }
+    }
+
+    private void prepareGuidingGpsRequired() {
+        CheckBoxPreference guidingGpsRequired = findPreference(Preferences.getKey(R.string.pref_KEY_B_GUIDING_GPS_REQUIRED));
         if (guidingGpsRequired != null) {
             guidingGpsRequired.setOnPreferenceChangeListener((preference, o) -> {
                 boolean newValue = (boolean) o;
@@ -73,10 +90,15 @@ public class SettingsLocationFragment extends PreferenceFragmentCompat {
                 if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
                     CheckBoxPreference status_bar = findPreference(Preferences.getKey(R.string.pref_KEY_B_STATUSBAR));
                     CheckBoxPreference screen_off = findPreference(Preferences.getKey(R.string.pref_KEY_B_RUN_SCREEN_OFF));
-                    if (Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P && screen_off.isChecked()) {
-                        status_bar.setEnabled(false);
+                    if (status_bar != null && screen_off != null) {
+                        if (Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P && screen_off.isChecked()) {
+                            status_bar.setEnabled(false);
+                        } else {
+                            status_bar.setEnabled(!newValue);
+                        }
                     } else {
-                        status_bar.setEnabled(!newValue);
+                        Logger.e(TAG, "One of the required settings was missing. (KEY_B_STATUSBAR," +
+                            " KEY_B_RUN_SCREEN_OFF)");
                     }
                 }
                 return true;
