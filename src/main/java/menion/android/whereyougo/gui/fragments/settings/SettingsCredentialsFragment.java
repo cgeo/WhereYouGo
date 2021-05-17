c8e80f1fb8278e8a737493c821cb88693d611a49
SchoolGuy <matrixfueller@gmail.com>
2021-05-17 08:27:21 +0200

    Implement button to check the login data

--- src/main/java/menion/android/whereyougo/gui/fragments/settings/SettingsCredentialsFragment.java
+++ src/main/java/menion/android/whereyougo/gui/fragments/settings/SettingsCredentialsFragment.java
@@ -3,9 +3,11 @@ package menion.android.whereyougo.gui.fragments.settings;
 import android.os.Bundle;
 
 import androidx.preference.EditTextPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import menion.android.whereyougo.R;
+import menion.android.whereyougo.network.LoginTask;
 import menion.android.whereyougo.preferences.Preferences;
 
 public class SettingsCredentialsFragment extends PreferenceFragmentCompat {
@@ -15,6 +17,7 @@ public class SettingsCredentialsFragment extends PreferenceFragmentCompat {
 
         EditTextPreference gc_username = findPreference(Preferences.getKey(R.string.pref_KEY_S_GC_USERNAME));
         EditTextPreference gc_password = findPreference(Preferences.getKey(R.string.pref_KEY_S_GC_PASSWORD));
+        Preference checkLogin = findPreference(Preferences.getKey(R.string.pref_KEY_S_GC_CHECK));
 
         if (gc_username != null) {
             gc_username.setOnPreferenceChangeListener((preference, o) -> {
@@ -25,6 +28,14 @@ public class SettingsCredentialsFragment extends PreferenceFragmentCompat {
         if (gc_password != null) {
             gc_password.setOnPreferenceChangeListener((preference, o) -> {
                 Preferences.GC_PASSWORD = (String) o;
+                return true;
+            });
+        }
+        if (checkLogin != null) {
+            checkLogin.setOnPreferenceClickListener(preference -> {
+                // TODO: Check for non empty username and password before
+                LoginTask loginTask = new LoginTask(Preferences.GC_USERNAME, Preferences.GC_PASSWORD);
+                loginTask.execute();
                 return true;
             });
         }
